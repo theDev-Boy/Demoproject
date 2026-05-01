d87f437b818c10ebfd8bdeb37c4db25631d310da
MuhammadAfnan02 <mustafa.afnan01@gmail.com>
2026-05-01 17:43:02 +0500

    fixed by Gemini 3.1 pro high

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -33,6 +33,8 @@ android {
     buildTypes {
         release {
             signingConfig = signingConfigs.getByName("debug")
+            isMinifyEnabled = false
+            isShrinkResources = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
